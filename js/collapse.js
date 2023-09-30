4c3e06761146f1eb7a686d6a97af498d6844adb6
IvanSaushin <ivan.saushin@gmail.com>
2023-10-01 00:37:24 +0300

    Выровнял header, обернул в контейнер. Добавил форму обратной связи

---
+++ js/collapse.js
@@ -0,0 +1,15 @@
+
+$("#phone").click(function () { $(this).setCursorPosition(1); }).mask("+7(999) 999-99-99");
+
+// var myCollapse = document.getElementById('myCollapse')
+// var bsCollapse = new bootstrap.Collapse(myCollapse, {
+//     toggle: false
+// })
+
+// var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
+// var collapseList = collapseElementList.map(function (collapseEl) {
+//   return new bootstrap.Collapse(collapseEl)
+// })
+
+
+// myCollapse.onclick(alert('hello'))
